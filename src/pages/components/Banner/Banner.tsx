81c3786a593eecb18f81725ad8a58ba1df7a452d
Felipe <fnasccunha@gmail.com>
2023-02-25 12:32:27 -0300

    add banner component

---
+++ src/pages/components/Banner/Banner.tsx
@@ -0,0 +1,28 @@
+import scss from './Banner.module.scss';
+
+function Banner() {
+  const bgCircleTop = [scss.bgCircle, scss.top];
+  const bgCircleBottom = [scss.bgCircle, scss.bottom];
+  const orangeDivider = [scss.divider, scss.orange];
+  const yellowDivider = [scss.divider, scss.yellow];
+
+  return (
+    <div className={scss.container}>
+      <div className={scss.gradient} />
+      <div className={bgCircleTop.join(' ')} />
+      <div className={scss.content}>
+        <div className={orangeDivider.join(' ')} />
+        <h1 className={scss.title}>
+          Fazemos a{' '}
+          <strong className={scss.strong}>
+            parte chata <span className={scss.bigger}>do seu negócio!</span>
+          </strong>
+        </h1>
+        <div className={yellowDivider.join(' ')} />
+      </div>
+      <div className={bgCircleBottom.join(' ')} />
+    </div>
+  );
+}
+
+export default Banner;
